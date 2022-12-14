493de8beccc74b70611987fd06604b29d87e0fd0
Corey Hawthorne <chawthorne02@gmail.com>
2022-12-14 15:35:57 -0500

    added homepage title and subtitle

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,7 +1,13 @@
-// import Header from "../Header/Header";
+import "../../styles/Homepage.css";
 
 function Homepage() {
-  return <div className="portfolio-title">Homepage</div>;
+  return (
+  <div className="homepage">
+    <section className="homepage-title">Corey Hawthorne</section>
+    <p className="homepage-subtitle">Software Developer looking to further improve my skills and further myself as a developer. I dabble in both the frontend and the backend. Always looking to learn more about new technologies   </p>
+  </div>
+  
+  )
 }
 
 export default Homepage;
