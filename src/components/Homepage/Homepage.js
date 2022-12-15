4aa16dda6c210ece866edd3d86e7e6ee3d5f1f35
Corey Hawthorne <chawthorne02@gmail.com>
2022-12-15 11:00:16 -0500

    added react icons to homepage

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,13 +1,33 @@
 import "../../styles/Homepage.css";
+import { BsGithub } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
 
 function Homepage() {
   return (
-  <div className="homepage">
-    <section className="homepage-title">Corey Hawthorne</section>
-    <p className="homepage-subtitle">Software Developer looking to further improve my skills and further myself as a developer. I dabble in both the frontend and the backend. Always looking to learn more about new technologies   </p>
-  </div>
-  
-  )
+    <div className="homepage">
+      <section className="homepage-title">Corey Hawthorne</section>
+      <p className="homepage-subtitle">
+        Software Developer looking to further improve my skills and further
+        myself as a developer. I dabble in both the frontend and the backend.
+        Always looking to learn more about new technologies
+      </p>
+      <section className="homepage-socials">
+        <a href="https://github.com/chawthorne02" target="_blank">
+          <BsGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/corey-hawthorne-80a2581b6/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+        <a href="chawthorne02@gmail.com">
+          <MdEmail />
+        </a>
+      </section>
+    </div>
+  );
 }
 
 export default Homepage;
