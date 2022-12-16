93d626d9854489d40897505c24050f0e851058dd
Corey Hawthorne <chawthorne02@gmail.com>
2022-12-16 12:49:45 -0500

    added carousel and about me text to the about me page

--- src/components/Aboutpage/Aboutpage.js
+++ src/components/Aboutpage/Aboutpage.js
@@ -1,5 +1,7 @@
 import Me from "../../images/Me.jpg";
 import Teacherme from "../../images/Teacherme.jpeg";
+import skydive from "../../images/skydive.jpeg";
+import graduationpic from "../../images/graduationpic.jpeg";
 import Carousel from "react-bootstrap/Carousel";
 import "../../styles/Aboutpage.css";
 
@@ -9,20 +11,39 @@ function Aboutpage() {
       <Carousel className="carousel">
         <Carousel.Item>
           <img className="about-img" src={Me} alt="First slide" />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="about-img" src={Teacherme} alt="Second slide" />
 
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>My classroom</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="about-img" src={skydive} alt="Third slide" />
+
+          <Carousel.Caption>
+            <h3>Skydiving</h3>
+            <p>Such an adrenaline rush!!</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="about-img" src={graduationpic} alt="Fourth slide" />
+
+          <Carousel.Caption>
+            <h3>My family</h3>
+            <p>My biggest support system. Oh yeah, Go Gamecocks!</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
+      <p className="about-text">
+        Changing careers from teaching to software developing was a big jump for
+        me. From leading the classroom to now being back on the other side just
+        showed how much I have a passion to learn and constantly better myself.
+        Joining Carolina Code School gave me that opportunity to keep pushing my
+        knowledge even further. Now that I have graduated, I am excited to
+        continue that learning process and see where it takes me.
+      </p>
     </div>
   );
 }
